484dcca1d1b9ac444065f9c8721fe3afeb0740be
nerenjanaprojects <nerenjana.projects@gmail.com>
2026-05-11 23:01:55 +0530

    completed alignment

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: '#e8622a',
+        'brand-dark': '#af3315',
+        'site-dark': '#1a2530',
+        'site-mid': '#1e2b35',
+        'sidebar-gray': '#5e656b',
+      },
+      letterSpacing: {
+        widest2: '0.3em',
+        widest3: '0.4em',
+      }
+    }
+  }
+}
